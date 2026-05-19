9c939c5e7235b7f1143ba0074babc41e3398089a
RajendraKumar08 <rkdbprjr343040@gmail.com>
2026-05-19 20:46:48 +0530

    Home page and footer

--- app/api/updateproduct/route.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/prisma-db";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req : NextRequest) {
-
-    return {"message" : "Hey this is updat item page"}
-    
-    
-}
